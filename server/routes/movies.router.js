a3790e2658e762b9d2d5e59147d28f9a4f94ca6f
Sean Olson <srolson@callutheran.edu>
2019-07-26 16:13:14 -0500

    server getting movies from db

--- server/routes/movies.router.js
+++ server/routes/movies.router.js
@@ -4,4 +4,16 @@ const pool = require('../modules/pool');
 
 // GET to db (get movies data, from movies table 
 
+router.get('/', (req, res) => {
+    const sqlText=`SELECT * FROM "movies"`;
+    pool.query(sqlText)
+      .then( (response) => {
+        res.send(response.rows);
+      })
+      .catch( (error) => {
+        console.log(`Error getting shows`, error);
+        res.sendStatus(500);
+      })
+  })
+
 module.exports = router;
