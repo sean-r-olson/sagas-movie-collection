26c3c259f59de5768a1f3b5169f3d50a622be2de
Sean Olson <srolson@callutheran.edu>
2019-07-28 12:36:02 -0500

    added edit route w/ buttons to navigate to edit page/back home

--- src/components/EditDetails/EditDetails.js
+++ src/components/EditDetails/EditDetails.js
@@ -5,10 +5,19 @@ import '../App/App.css';
 
 class EditDetails extends Component {
 
+  
+
   render() {
     return (
       <div>
          <h1 className="App-header">Edit Details</h1>
+         <button onClick={this.cancel}>Cancel</button>
+         <button onClick={this.save}>Save</button>
+         <form>
+           <input type="text" placeholder="Edit Movie Title"></input>
+           <br/>
+           <textarea type="text" placeholder="Edit Movie Description"></textarea>
+         </form>
       </div>
     )
   }
--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -5,10 +5,21 @@ import '../App/App.css';
 
 class MovieDetails extends Component {
 
+  homePage = () => {
+    this.props.history.push('/');
+  }
+
+  editDetails = () => {
+    this.props.history.push('/edit');
+  }
+
   render() {
     console.log(this.props.reduxStore.movieDescription)
     return (
       <div>
+        <h1 className="App-header">Movie Details</h1>
+        <button onClick={this.homePage}>Back to Movies</button>
+        <button onClick={this.editDetails}>Edit Details</button>
         <h3>{this.props.reduxStore.movieDescription.title}</h3>
         <p>{this.props.reduxStore.movieDescription.description}</p>
         <p>{this.props.reduxStore.movieDescription.name}</p>
